bd809d1335009105a3559325aa379df411bd2162
Thiagod3 <thiago.vilela3@outlook.com>
2024-07-03 15:17:55 -0300

    created ProdutoVenda

---
+++ src/main/java/com/compass/thiagofv/domain/ProdutoVenda.java
@@ -0,0 +1,81 @@
+package com.compass.thiagofv.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ProdutoVenda implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	private ProdutoVendaPK id = new ProdutoVendaPK();
+	
+	private Integer quantidade;
+	private Double precoUn;
+	
+	
+	public ProdutoVenda(Produto produto, Venda venda, Integer quantidade, Double precoUn) {
+		id.setProduto(produto);
+		id.setVenda(venda);
+		this.quantidade = quantidade;
+		this.precoUn = precoUn;
+	}
+
+
+	public Produto getProduto() {
+		return id.getProduto();
+	}
+	
+	public Venda getVenda() {
+		return id.getVenda();
+	}
+
+	public ProdutoVendaPK getId() {
+		return id;
+	}
+
+
+	public void setId(ProdutoVendaPK id) {
+		this.id = id;
+	}
+
+
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+
+
+	public Double getPrecoUn() {
+		return precoUn;
+	}
+
+
+	public void setPrecoUn(Double precoUn) {
+		this.precoUn = precoUn;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProdutoVenda other = (ProdutoVenda) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+}
