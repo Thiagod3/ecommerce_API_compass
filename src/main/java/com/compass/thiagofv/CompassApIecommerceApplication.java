56a4b0b0a007020a2bd488575cbbe1d5196d8989
Thiagod3 <thiago.vilela3@outlook.com>
2024-07-03 14:09:23 -0300

    setup

---
+++ src/main/java/com/compass/thiagofv/CompassApIecommerceApplication.java
@@ -0,0 +1,13 @@
+package com.compass.thiagofv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompassApIecommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompassApIecommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compass/thiagofv/CompassApIecommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compass.thiagofv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompassApIecommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
